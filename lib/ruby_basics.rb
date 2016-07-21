3d3296525378555778243fcc5197e7715543f67c
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2016-07-21 19:15:25 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,39 @@
 def division(num1, num2)
- 
+  num1 / num2
 end
+
+division(12, 2)
 
 def assign_variable(value)
-  
+  value
 end
 
-def argue
-  
+assign_variable("Bob")
+
+def argue(phrase)
+  phrase
 end
 
-def greeting
+argue("I'm right and you are wrong!")
+
+def greeting(greeting, name )
 
 end
 
 def return_a_value
-
+  "Nice"
 end
+
+return_a_value
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
+last_evaluated_value
 
+def pizza_party(pizza = "cheese")
+  pizza
 end
+
+pizza_party("pepperoni")
